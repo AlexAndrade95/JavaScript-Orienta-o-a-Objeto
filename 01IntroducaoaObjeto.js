bd06df76756a6f118baf4ec4441999fd4eb1f5ca
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-30 13:03:07 -0300

    commit 01 versao 1.2

---
+++ 01IntroducaoaObjeto.js
@@ -0,0 +1,25 @@
+/* Introducao a Objeto
+"Vice presidente de Javascript !!! " */
+
+//CODIGO NAO EXECUTAVEL!!!
+
+//linguagem Procedural (exemplo linguagem C )
+processamento(valor1, valor2, valor3)
+
+//OO //estrutura que agrupa dados e processamentos 
+ objeto = {
+     valor1,
+     valor2,
+     valor3,
+     processamento() {
+         // ...
+     }
+ }
+
+ objeto.processamento() //Foco passou a ser o objeto
+
+ //Principios importantes:
+ //1. abstracao           //pegar objetos reais para um sistema.
+ //2. encapsulamento      //detalhes que ficam escondidos nos sistemas
+ //3. heranca (prototype) //receber atributos de objeto pai (Em JS somente um pai,podendo avo...)
+ //4. polimorfismo        //multiplas formas
